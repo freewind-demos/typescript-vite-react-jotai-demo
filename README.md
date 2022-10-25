69837598df9f1817e371d0cbe1ac4debf90fe221
Freewind <nowindlee@gmail.com>
2022-10-25 14:54:42 +0800

    typescript-vite-react-jotai-demo

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-TypeScript Vite React Demo
+TypeScript Vite React Jotai Demo
 ===========================
 
 ```
 npm install
-npm run demo
+npm start
 ```
 
 It will open page on browser automatically.
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,19 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import './Hello.pcss';
+import {atom, useAtom} from 'jotai';
 
 type Props = {};
 
+const atoms = {
+  name: atom('jotai')
+}
+
 export const Hello: FC<Props> = ({}) => {
+  const [name, setName] = useAtom(atoms.name)
   return <div className={'Hello'}>
-    <h1>Hello React</h1>
+    <h1>Hello Jotai</h1>
+    <input value={name} onChange={(event) => setName(event.target.value)}/>
+    <hr/>
+    {name}
   </div>;
 }
